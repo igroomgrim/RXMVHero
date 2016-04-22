6ce6c0b1c87840f9e6050baadf90b7a01c1949ae
Anak Mirasing <thaihooligan@hotmail.com>
2016-04-22 11:22:06 +0700

    add constant file for store api key

---
+++ RXMVHero/Helper/MVConstants.swift
@@ -0,0 +1,12 @@
+//
+//  MVConstants.swift
+//  RXMVHero
+//
+//  Created by Anak Mirasing on 4/22/2559 BE.
+//  Copyright © 2559 iGROOMGRiM. All rights reserved.
+//
+
+import Foundation
+
+let MVPublicKey = ""
+let MVPrivateKey = ""
